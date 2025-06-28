d889ad84c0c309704b3003d495129b20da8d699f
cankocak <can_kocak1@epam.com>
2025-06-28 20:07:37 +0300

    added post request

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,25 @@ const users = [
     firstName: "John",
     lastName: "Doe",
     age: 25
+  },
+  {
+    firstName: "Can",
+    lastName: "Kocak",
+    age: 27
   }
 ]
 
 // all routes in here starts with /users
 router.get('/', (req, res) => {
-  console.log(users);
-  res.send("Hello");
+  res.send(users);
 })
+
+router.post('/', (req, res) => {
+  const user = req.body;
+
+  users.push(user);
+
+  res.send(`User with the name ${user.firstName} added to the database!`);
+});
 
 export default router;
