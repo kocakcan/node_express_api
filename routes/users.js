bcd8d44e2c5f1a74ce32d302a32537ea85cfbf99
cankocak <can_kocak1@epam.com>
2025-07-02 16:55:19 +0300

    added DELETE ❌

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,12 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 // all routes in here starts with /users
 router.get('/', (req, res) => {
   res.send(users);
-})
+});
 
 router.post('/', (req, res) => {
   const user = req.body;
@@ -24,6 +24,14 @@ router.get('/:id', (req, res) => {
   const foundUser = users.find((user) => user.id === id);
 
   res.send(foundUser);
-})
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with the id ${id} has been deleted from the database!`);
+});
 
 export default router;
